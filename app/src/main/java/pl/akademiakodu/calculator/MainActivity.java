1149fe7e0837fa2319155c1a8e905908d16885d3
Lukasz W <lukasz.wiszniewski007@gmail.com>
2016-09-10 17:03:01 +0200

    MainActivity.java:   refactoring of the code

--- app/src/main/java/pl/akademiakodu/calculator/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/calculator/MainActivity.java
@@ -13,14 +13,16 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
     Button buttonC, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonDel, buttonMulti, buttonMinus, buttonAdd, buttonEqu;
     StringBuffer readLine = new StringBuffer();
-    int firstNumber,secondNumber,endOfFirstNumber,interresult;
+    int firstNumber, secondNumber, endOfFirstNumber, interresult;
     Integer result;
     String charArit;
-    @BindView(R.id.progressBar)public ProgressBar progressBar;
+    @BindView(R.id.progressBar)
+    public ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonAdd = (Button) findViewById((R.id.buttonAdd));
         buttonMinus = (Button) findViewById((R.id.buttonMinus));
         buttonEqu = (Button) findViewById((R.id.buttonEqu));
-        readLine.delete(0,readLine.length());
+        readLine.delete(0, readLine.length());
 
         ButterKnife.bind(this);
 
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        należałoby zmienić jeszcze:
        -if'y na Switch
        -możnaby również oprogramować button'y metodami public void buttonProcedure(String digit){}
-       - poległem z obsługą błedu dzielenia przez 0 i za dużo czasu straciłem żeby zrefaktoryzować resztę kodu
+       - poległem z obsługą błedu dzielenia przez 0 i za dużo czasu na tym straciłem, toteż zabrakło czasu zeby zrefaktoryzować resztę kodu
        -powinnien być zmieniony również layout ale po zmianie na inny trzeba wszystko od nowa poustawiać na ekranie, więc zrezygnowałem
          */
 
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     //public String a= textView.getText().toString();
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         /*switch (v.getId()){
             case R.id.button0:
                 readLine.append("0");
@@ -72,11 +74,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 textView.setText(readLine.toString());
         }*/
         if (button0.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("0");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -86,25 +88,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button1.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("1");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else{
-                readLine.delete(0,readLine.length());
+            } else {
+                readLine.delete(0, readLine.length());
                 textView.setText("");
-                result=null;
+                result = null;
                 readLine.append("1");
                 textView.setText(readLine.toString());
                 enableButtons();
             }
         }
         if (button2.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("2");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -114,11 +116,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button3.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("3");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -128,11 +130,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button4.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("4");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -142,11 +144,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button5.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("5");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -156,11 +158,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button6.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("6");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -170,11 +172,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button7.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("7");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -184,11 +186,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button8.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("8");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -198,11 +200,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (button9.getId() == v.getId()) {
-            if (result==null){
+            if (result == null) {
                 readLine.append("9");
                 textView.setText(readLine.toString());
                 enableButtons();
-            }else {
+            } else {
                 readLine.delete(0, readLine.length());
                 textView.setText("");
                 result = null;
@@ -213,43 +215,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (buttonDel.getId() == v.getId()) {
-            firstNumber=Integer.parseInt(readLine.toString());
-            endOfFirstNumber=readLine.length();
+            firstNumber = Integer.parseInt(readLine.toString());
+            endOfFirstNumber = readLine.length();
             readLine.append("/");
-            charArit="/";
+            charArit = "/";
             textView.setText(readLine.toString());
             buttonEqu.setEnabled(true);
         }
         if (buttonMulti.getId() == v.getId()) {
-            firstNumber=Integer.parseInt(readLine.toString());
-            endOfFirstNumber=readLine.length();
+            firstNumber = Integer.parseInt(readLine.toString());
+            endOfFirstNumber = readLine.length();
             readLine.append("*");
-            charArit="*";
+            charArit = "*";
             textView.setText(readLine.toString());
             buttonEqu.setEnabled(true);
         }
         if (buttonMinus.getId() == v.getId()) {
-            firstNumber=Integer.parseInt(readLine.toString());
-            endOfFirstNumber=readLine.length();
+            firstNumber = Integer.parseInt(readLine.toString());
+            endOfFirstNumber = readLine.length();
             readLine.append("-");
-            charArit="-";
+            charArit = "-";
             textView.setText(readLine.toString());
             buttonEqu.setEnabled(true);
         }
         if (buttonAdd.getId() == v.getId()) {
-            firstNumber=Integer.parseInt(readLine.toString());
-            endOfFirstNumber=readLine.length();
+            firstNumber = Integer.parseInt(readLine.toString());
+            endOfFirstNumber = readLine.length();
             readLine.append("+");
-            charArit="+";
+            charArit = "+";
             textView.setText(readLine.toString());
             buttonEqu.setEnabled(true);
         }
         if (buttonC.getId() == v.getId()) {
             textView.setText("");
-            readLine.delete(0,readLine.length());
+            readLine.delete(0, readLine.length());
         }
         if (buttonEqu.getId() == v.getId()) {
-            secondNumber=Integer.parseInt(readLine.substring(endOfFirstNumber+1));
+            secondNumber = Integer.parseInt(readLine.substring(endOfFirstNumber + 1));
             NewTask newTask = new NewTask();
             newTask.execute();
             progressBar.setProgress(0);
@@ -257,85 +259,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             disableButtons();
         }
     }
-    public void enableButtons(){
+
+    public void enableButtons() {
         buttonAdd.setEnabled(true);
         buttonDel.setEnabled(true);
         buttonMinus.setEnabled(true);
         buttonMulti.setEnabled(true);
     }
-    public void disableButtons(){
+
+    public void disableButtons() {
         buttonAdd.setEnabled(false);
         buttonDel.setEnabled(false);
         buttonMinus.setEnabled(false);
         buttonMulti.setEnabled(false);
     }
+
+    public void alertDialog(String message) {
+        AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog2.setMessage(message);
+        alertDialog2.setTitle("error");
+        alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog2.show();
+    }
+
     public class NewTask extends AsyncTask<Void, Integer, Integer> {
 
         @Override
-        protected Integer doInBackground(Void... params) {
-            for( int i=0;i<=1;i++){
+        protected Integer doInBackground(Void... params) throws ArithmeticException{
+            for (int i = 0; i <= 1; i++) {
                 publishProgress(i);
-            try {
-                Thread.sleep(150);
-                switch(charArit) {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-":
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "/":
-                        try {
-                            result = firstNumber / secondNumber;
-                        }catch(ArithmeticException e){
-                            AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
-                            alertDialog2.setMessage(e.getMessage());
-                            alertDialog2.setTitle("error");
-                            alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                        }
-                                    });
-                            alertDialog2.show();
-                        }
-                        break;
-                /*case "/":
-                    try {
-                        if(secondNumber==0)throw new ZeroDivideException("Cannot divide by 0");
-                        else result = firstNumber / secondNumber;
+                try {
+                    Thread.sleep(150);
+                    switch (charArit) {
+                        case "+":
+                            result = firstNumber + secondNumber;
+                            break;
+                        case "-":
+                            result = firstNumber - secondNumber;
+                            break;
+                        case "/":
+                            try {
+                                if (secondNumber == 0) {
+                                    throw new ArithmeticException();
+                                } else {
+                                    result = firstNumber / secondNumber;
+                                }
+                            } catch (ArithmeticException e) {
+                                alertDialog("Forbidden dividing by zero");
+                            }
+                            break;
 
-                    }catch(ZeroDivideException e){
-                        AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
-                        alertDialog2.setMessage(e.getMessage());
-                        alertDialog2.setTitle("error");
-                        alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        alertDialog2.show();
+                        case "*":
+                            result = firstNumber * secondNumber;
+                            break;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                    break;*/
-                    case "*":
-                        result=firstNumber*secondNumber;
-                        break;
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             }
             return null;
         }
 
         @Override
-        public void onPostExecute(Integer args){
-            readLine.append("="+result.toString());
+        public void onPostExecute(Integer args) {
+            readLine.append("=" + result.toString());
             textView.setText(readLine.toString());
         }
+
         @Override
-        public void onProgressUpdate(Integer... args){
+        public void onProgressUpdate(Integer... args) {
             progressBar.setProgress(args[0]);
         }
     }
